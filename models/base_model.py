e60d1fa9f9da68f1ed7baef2a42c45bf53f8011b
4ions <leovalenciasantana@gmail.com>
2021-02-10 09:41:08 -0500

    base_models add

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module of Base class """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ Class Base Model """
+    
+    def __init__(self):
+        """ Init """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.update_at = datetime.now()
+
