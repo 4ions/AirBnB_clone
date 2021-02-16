c0f8ccfe087260f5675054451a0c6e47386a6a82
4ions <leovalenciasantana@gmail.com>
2021-02-16 10:36:37 -0500

    Remove second line

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Import module storage"""
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
 storage = FileStorage()
 storage.reload()
