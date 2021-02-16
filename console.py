0156b1e2b19143ca94350ee3a537086b8f6289ce
7honarias <7honarias@gmail.com>
2021-02-16 15:13:22 +0000

    [UPDATE] - correction

--- console.py
+++ console.py
@@ -21,12 +21,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """ Quit command to exit the program
+        """
+        Quit command to exit the program
          """
+        print()
         return True
 
     def do_EOF(self, line):
-        """ EFO """
+        """
+        EFO """
         print()
         return True
 
